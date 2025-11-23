6fe46ac2952eb75af34b4948f0abaee903336ef3
aras <arasrasoolzadeh@gmail.com>
2025-11-23 15:08:30 +0330

    Creating a course class: Add Course model including Id, Title, Teacher relation, student list, and grade dictionary

---
+++ SchoolManagementSystem/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public Dictionary<int, int> Grades { get; set; } = new Dictionary<int, int>();
+
+        public override string ToString()
+        {
+            return $"{Id} - {Title} (Teacher: {Teacher?.FullName ?? "No Teacher"})";
+        }
+    }
+}
