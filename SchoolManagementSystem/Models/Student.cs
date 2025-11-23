9389beae598b9d7bbcdace8e7fa1ab5b9c36552d
aras <arasrasoolzadeh@gmail.com>
2025-11-23 10:22:03 +0330

    Creating a student class: Add Student model with basic properties (Id, FullName, StudentNumber, BirthDate, Major)

---
+++ SchoolManagementSystem/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }                   
+        public string FirstName { get; set; }          
+        public string LastName { get; set; }           
+        public string StudentNumber { get; set; }      
+        public DateTime BirthDate { get; set; }        
+        public string Major { get; set; }              
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName} - ({StudentNumber})";
+        }
+    }
+}
