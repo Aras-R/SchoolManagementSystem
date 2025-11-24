44045622a9e745f55754ab5fdfb58ff9e51ce21a
aras <arasrasoolzadeh@gmail.com>
2025-11-24 09:35:28 +0330

    Add AddTeacherService for creating new teachers

---
+++ SchoolManagementSystem/Services/Teachers/AddTeacherService.cs
@@ -0,0 +1,40 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Teachers
+{
+    public class AddTeacherService
+    {
+        private readonly DataContext _context;
+        public AddTeacherService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Teacher Add(string teacherCode, string firstName, string lastName, string college)
+        {
+            int newId = _context.Teachers.Count == 0
+                ? 1
+                : _context.Teachers.Max(t => t.Id) + 1;
+
+            var teacher = new Teacher
+            {
+                Id = newId,
+                TeacherCode = teacherCode,
+                FirstName = firstName,
+                LastName = lastName,
+                College = college
+            };
+
+            _context.Teachers.Add(teacher);
+
+            return teacher;
+
+        }
+    }
+}
