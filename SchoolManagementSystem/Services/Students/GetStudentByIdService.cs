7ee1cabf936eefbd04d1f33e8c1ccaaea7782224
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:44:08 +0330

    Add GetStudentByIdService to retrieve a student by Id

---
+++ SchoolManagementSystem/Services/Students/GetStudentByIdService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Students
+{
+    public class GetStudentByIdService
+    {
+        private readonly DataContext _Context;
+        public GetStudentByIdService(DataContext context)
+        {
+            _Context = context;
+        }
+
+        public Student? Get(int id)
+        {
+            return _Context.Students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
