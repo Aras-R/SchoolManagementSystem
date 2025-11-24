ef047ea1cabc7e6d4145a4cb9621b84781c8a5c4
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:37:46 +0330

    Add AddStudentService with automatic Id generation and student creation logic

---
+++ SchoolManagementSystem/Services/Students/AddStudentService.cs
@@ -0,0 +1,40 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Students
+{
+    public class AddStudentService
+    {
+        private readonly DataContext _context;
+        public AddStudentService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Student Add(string firstName, string lastName, string studentNumber, DateTime birthDate, string major)
+        {
+            int newId = _context.Students.Count == 0
+                ? 1
+                : _context.Students.Max(s => s.Id) + 1;
+
+            var student = new Student
+            {
+                Id = newId,
+                FirstName = firstName,
+                LastName = lastName,
+                StudentNumber = studentNumber,
+                BirthDate = birthDate,
+                Major = major
+            };
+
+            _context.Students.Add(student);
+
+            return student;
+        }
+    }
+}
