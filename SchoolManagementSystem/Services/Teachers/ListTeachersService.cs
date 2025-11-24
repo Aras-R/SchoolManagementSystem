be5c816827bf2c00d40587cf8aa9900eb32868b2
aras <arasrasoolzadeh@gmail.com>
2025-11-24 09:38:41 +0330

    Add ListTeachersService to return all teachers

---
+++ SchoolManagementSystem/Services/Teachers/ListTeachersService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Teachers
+{
+    public class ListTeachersService
+    {
+        private readonly DataContext _Context;  
+        public ListTeachersService(DataContext context)
+        {
+            _Context = context;
+        }
+
+        public List<Teacher> GetAll()
+        {
+            return _Context.Teachers.ToList();
+        }
+    }
+}
