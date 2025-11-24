8bc5630489bf29498d3dc27b74d887c4060c8c04
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:49:04 +0330

    Add EditStudentService to update existing student information

---
+++ SchoolManagementSystem/Services/Students/EditStudentService.cs
@@ -0,0 +1,34 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Students
+{
+    public class EditStudentService
+    {
+        private readonly DataContext _Context;
+        public EditStudentService(DataContext Context)
+        {
+            _Context = Context;
+        }
+
+        public Student? Edit(int id, string firstName, string lastName, string studentNumber, DateTime birthDate, string major)
+        {
+            var student = _Context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return null;
+
+            student.FirstName = firstName;
+            student.LastName = lastName;
+            student.StudentNumber = studentNumber;
+            student.BirthDate = birthDate;
+            student.Major = major;
+
+            return student;
+        }
+    }
+}
