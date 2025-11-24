f5b55a1555a43873d027093882627cfb64520545
aras <arasrasoolzadeh@gmail.com>
2025-11-24 09:41:17 +0330

    Add GetTeacherByIdService for retrieving teacher by Id

---
+++ SchoolManagementSystem/Services/Teachers/GetTeacherByIdService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Teachers
+{
+    public class GetTeacherByIdService
+    {
+        private readonly DataContext _context;
+        public GetTeacherByIdService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Teacher? GetById(int id)
+        {
+            return _context.Teachers.FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
