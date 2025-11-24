ce929b00fd06dec4a0f5d77bd0a1b2d10639887f
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:53:01 +0330

    Add DeleteStudentService to remove a student by Id

---
+++ SchoolManagementSystem/Services/Students/DeleteStudentService.cs
@@ -0,0 +1,30 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Students
+{
+    public class DeleteStudentService
+    {
+        private readonly DataContext _Context;
+        public DeleteStudentService(DataContext context)
+        {
+            _Context = context;
+        }
+
+        public bool Delete(int id)
+        {
+            var student = _Context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return false;
+            }
+
+            _Context.Students.Remove(student);
+            return true;
+        }
+    }
+}
