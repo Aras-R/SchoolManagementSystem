989958cb5056d63f1ca7c6d78b5289f78cb36431
aras <arasrasoolzadeh@gmail.com>
2025-11-24 09:45:16 +0330

    Add DeleteTeacherService to remove teacher by Id

---
+++ SchoolManagementSystem/Services/Teachers/DeleteTeacherService.cs
@@ -0,0 +1,30 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Teachers
+{
+    public class DeleteTeacherService
+    {
+        private readonly DataContext _context;
+
+        public DeleteTeacherService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool Delete(int id)
+        {
+            var teacher = _context.Teachers.FirstOrDefault(t => t.Id == id);
+            if (teacher == null)
+                return false;
+
+            _context.Teachers.Remove(teacher);
+
+            return true;
+        }
+    }
+}
