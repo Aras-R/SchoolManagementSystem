c7a990f777f245d1ca22e758e4f6820549c3ac24
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:27:56 +0330

    Add DataContext for in-memory data storage

---
+++ SchoolManagementSystem/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Data
+{
+    public class DataContext
+    {
+        public List<Student> Students { get; set; } = new();
+        public List<Teacher> Teachers { get; set; } = new();
+        public List<Course> Courses { get; set; } = new();
+    }
+}
