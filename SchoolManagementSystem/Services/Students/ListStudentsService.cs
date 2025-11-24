53971694871498d2d7b8695c7c8db562f1101df4
aras <arasrasoolzadeh@gmail.com>
2025-11-24 08:40:58 +0330

    Add ListStudentsService to retrieve all students from DataContext

---
+++ SchoolManagementSystem/Services/Students/ListStudentsService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Students
+{
+    public class ListStudentsService
+    {
+        private readonly DataContext _Context;
+        public ListStudentsService(DataContext dataContext)
+        {
+            _Context = dataContext;
+        }
+
+        public List<Student> GetAll()
+        {
+            return _Context.Students;
+        }
+    }
+}
