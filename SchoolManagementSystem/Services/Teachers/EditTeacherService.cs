1fc5198ca6b1f3ed4b0a4e0d0786d2a8feac109e
aras <arasrasoolzadeh@gmail.com>
2025-11-24 09:43:52 +0330

    Add EditTeacherService to update teacher information

---
+++ SchoolManagementSystem/Services/Teachers/EditTeacherService.cs
@@ -0,0 +1,32 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Teachers
+{
+    public class EditTeacherService
+    {
+        private readonly DataContext _context;
+        public EditTeacherService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool Edit(int id, string teacherCode, string firstName, string lastName, string college)
+        {
+            var teacher = _context.Teachers.FirstOrDefault(t => t.Id == id);
+            if (teacher == null)
+                return false;
+
+            teacher.TeacherCode = teacherCode;
+            teacher.FirstName = firstName;
+            teacher.LastName = lastName;
+            teacher.College = college;
+
+            return true;
+        }
+    }
+}
