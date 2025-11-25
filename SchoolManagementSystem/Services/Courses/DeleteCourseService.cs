189e35e4ebe72a2cdac06bfbdaa0197f3b9cf837
aras <arasrasoolzadeh@gmail.com>
2025-11-25 13:55:05 +0330

    Add DeleteCourseService to remove courses from the system

---
+++ SchoolManagementSystem/Services/Courses/DeleteCourseService.cs
@@ -0,0 +1,28 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Courses
+{
+    public class DeleteCourseService
+    {
+        private readonly DataContext _context;
+        public DeleteCourseService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool Delete(int courseId)
+        {
+            var course = _context.Courses.FirstOrDefault(c => c.Id == courseId);
+            if (course == null)
+                return false;
+
+            _context.Courses.Remove(course);
+            return true;
+        }
+    }
+}
