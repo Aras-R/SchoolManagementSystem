56bc027e19ea3204e6d3b6eaf5283e72ab536d1b
aras <arasrasoolzadeh@gmail.com>
2025-11-25 08:35:45 +0330

    Add AddCourseService to create new courses with assigned teacher

---
+++ SchoolManagementSystem/Services/Courses/AddCourseService.cs
@@ -0,0 +1,45 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Courses
+{
+    public class AddCourseService
+    {
+        private readonly DataContext _context;
+
+        public AddCourseService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Course Add(string title, int teacherId)
+        {
+
+            var teacher = _context.Teachers.FirstOrDefault(t => t.Id == teacherId);
+            if (teacher == null)
+                throw new Exception("Teacher not found.");
+
+
+            int newId = _context.Courses.Count == 0
+                ? 1
+                : _context.Courses.Max(c => c.Id) + 1;
+
+
+            var course = new Course
+            {
+                Id = newId,
+                Title = title,
+                Teacher = teacher,
+            };
+
+            _context.Courses.Add(course);
+
+            return course;
+        }
+    }
+}
