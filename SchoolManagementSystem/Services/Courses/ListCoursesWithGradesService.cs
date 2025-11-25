4c5cb978457e16df16aacabfd4cdd300bcfd4480
aras <arasrasoolzadeh@gmail.com>
2025-11-25 13:08:23 +0330

    Add ListCoursesWithGradesService to retrieve full course list with students and grades

---
+++ SchoolManagementSystem/Services/Courses/ListCoursesWithGradesService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Courses
+{
+    public class ListCoursesWithGradesService
+    {
+        private readonly DataContext _context;
+        public ListCoursesWithGradesService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public List<Course> GetAll()
+        {
+            return _context.Courses;
+        }
+    }
+}
