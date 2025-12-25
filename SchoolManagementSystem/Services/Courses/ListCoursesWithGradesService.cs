e99247722062da230c6ca8bec3e90aee66ad87d0
aras <arasrasoolzadeh@gmail.com>
2025-12-25 11:48:19 +0330

    Rewrite ListCoursesWithGradesService to use StudentCourse entity

--- SchoolManagementSystem/Services/Courses/ListCoursesWithGradesService.cs
+++ SchoolManagementSystem/Services/Courses/ListCoursesWithGradesService.cs
@@ -11,15 +11,57 @@ namespace SchoolManagementSystem.Services.Courses
     public class ListCoursesWithGradesService
     {
         private readonly DataContext _context;
+
         public ListCoursesWithGradesService(DataContext context)
         {
             _context = context;
         }
 
-        public List<Course> GetAll()
+        // Get all courses with enrolled students and their grades
+        public List<CourseWithStudentsAndGrades> GetAll()
         {
-            // Return full list of courses
-            return _context.Courses;
+            var result = new List<CourseWithStudentsAndGrades>();
+
+            foreach (var course in _context.Courses)
+            {
+                // Find all StudentCourse entries for this course
+                var studentCourses = _context.StudentCourses
+                    .Where(sc => sc.CourseId == course.Id)
+                    .ToList();
+
+                var studentsWithGrades = studentCourses
+                    .Select(sc =>
+                    {
+                        var student = _context.Students.FirstOrDefault(s => s.Id == sc.StudentId);
+                        return new StudentGrade
+                        {
+                            Student = student!,
+                            Grade = sc.Grade
+                        };
+                    }).ToList();
+
+                result.Add(new CourseWithStudentsAndGrades
+                {
+                    Course = course,
+                    StudentsWithGrades = studentsWithGrades
+                });
+            }
+
+            return result;
         }
+    }
+
+    // Helper class to return student + grade
+    public class StudentGrade
+    {
+        public Student Student { get; set; }
+        public int? Grade { get; set; }
+    }
+
+    // Helper class to return course + its students + grades
+    public class CourseWithStudentsAndGrades
+    {
+        public Course Course { get; set; }
+        public List<StudentGrade> StudentsWithGrades { get; set; } = new();
     }
 }
