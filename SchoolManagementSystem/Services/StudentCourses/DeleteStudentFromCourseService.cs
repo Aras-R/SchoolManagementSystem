d284a43da88ed44f8b32f74d6196744d69908273
aras <arasrasoolzadeh@gmail.com>
2025-12-25 11:31:22 +0330

    Add DeleteStudentFromCourseService to remove a student from a course

---
+++ SchoolManagementSystem/Services/StudentCourses/DeleteStudentFromCourseService.cs
@@ -0,0 +1,34 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.StudentCourses
+{
+    public class DeleteStudentFromCourseService
+    {
+        private readonly DataContext _context;
+
+        public DeleteStudentFromCourseService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Remove a student from a course
+        public bool Delete(int studentId, int courseId)
+        {
+            // Find the StudentCourse entry
+            var studentCourse = _context.StudentCourses
+                .FirstOrDefault(sc => sc.StudentId == studentId && sc.CourseId == courseId);
+
+            if (studentCourse == null)
+                return false; 
+
+            
+            _context.StudentCourses.Remove(studentCourse);
+            return true;
+        }
+    }
+}
