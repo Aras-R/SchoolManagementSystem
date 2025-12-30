3586c1a0a5a72a121f4be54b67debfb1043d3fcf
aras <arasrasoolzadeh@gmail.com>
2025-12-30 11:24:47 +0330

    Adding a student ayerage grade calculate service

---
+++ SchoolManagementSystem/Services/Calculations/CalculateStudentAverageService.cs
@@ -0,0 +1,32 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Calculations
+{
+    public class CalculateStudentAverageService
+    {
+        private readonly DataContext _context;
+        public CalculateStudentAverageService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Calculates average grade of a student
+        public double? Calculate(int studentId)
+        {
+            var grades = _context.StudentCourses
+                .Where(sc => sc.StudentId == studentId && sc.Grade.HasValue)
+                .Select(sc => sc.Grade!.Value)
+                .ToList();
+
+            if (!grades.Any())
+                return null;
+
+            return grades.Average();
+        }
+    }
+}
