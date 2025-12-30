0650176de4409b183f6f987973fa9d7ffeac8a4f
aras <arasrasoolzadeh@gmail.com>
2025-12-30 11:37:34 +0330

    Adding a Overall average grade calculate service

---
+++ SchoolManagementSystem/Services/Calculations/CalculateOverallAverageService.cs
@@ -0,0 +1,32 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Calculations
+{
+    public class CalculateOverallAverageService
+    {
+        private readonly DataContext _context;
+        public CalculateOverallAverageService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Calculates average grade of all students
+        public double? Calculate()
+        {
+            var grades = _context.StudentCourses
+                .Where(sc => sc.Grade.HasValue)
+                .Select(sc => sc.Grade!.Value)
+                .ToList();
+
+            if (!grades.Any())
+                return null;
+
+            return grades.Average();
+        }
+    }
+}
