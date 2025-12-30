8723f4b8c7be572762d48b16c9065bdfddb400e1
aras <arasrasoolzadeh@gmail.com>
2025-12-30 11:30:15 +0330

    Adding a cuorse average grade calculate service

---
+++ SchoolManagementSystem/Services/Calculations/CalculateCourseAverageService.cs
@@ -0,0 +1,32 @@
+﻿using SchoolManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Calculations
+{
+    public class CalculateCourseAverageService
+    {
+        private readonly DataContext _context;
+        public CalculateCourseAverageService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Calculates average grade of a course
+        public double? Calculate(int courseId)
+        {
+            var grades = _context.StudentCourses
+                .Where(sc => sc.CourseId == courseId && sc.Grade.HasValue)
+                .Select(sc => sc.Grade!.Value)
+                .ToList();
+
+            if (!grades.Any())
+                return null;
+
+            return grades.Average();
+        }
+    }
+}
